e19cb930ce2ac06bf02233720feece1533f3f7de
dealdrain <dealdrain@yahoo.com>
2023-06-22 07:31:37 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers zero to nine
+ * Return: zero to nine followed by a new line
+ *
+ */
+
+void print_numbers(void)
+{
+	int b;
+
+	for (b >= 0, b <= 9, b++)
+
+	{
+		_putchar (b + '0');
+	}
+	_putchar('\n');
+}
