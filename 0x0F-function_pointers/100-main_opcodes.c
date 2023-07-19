dfc28376d6a94a11ed081773529de73da3484f68
dealdrain <dealdrain@yahoo.com>
2023-07-19 11:21:45 -0700

    Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <udis86.h>
+
+/**
+  * main - ...
+  * @argc: arg count.
+  * @argv: str lgt..
+  *
+  * Return: nothin.
+  */
+
+int main(int argc, char *argv[])
+{
+	ud_t ud_obj;
+	int t = 0;
+	int x = 0;
+
+	if (argc == 2)
+	{
+		t = atoi(argv[1]);
+
+		if (t < 0)
+		{
+			printf("Error\n");
+			exit(2);
+		}
+
+		ud_unit(&ud_obj);
+		ud_set_input_buffer(&ud_obj, argv[1], t);
+		ud_set_mode(&ud_obj, 64);
+		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
+
+		while (ud_disassemble(&ud_obj))
+		{
+			printf("\t%s\n", ud_insn_hex(&ud_obj));
+		}
+	}
+
+	return (0);
+}
+
