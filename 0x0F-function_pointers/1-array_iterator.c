2130810e9628af668068cc167a426d063bd3ccc6
dealdrain <dealdrain@yahoo.com>
2023-07-19 10:09:22 -0700

    If you spend too much time thinking about a thing, you_ll never get it done

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function on elements of array
+ * @array: array of int
+ * @size: array size
+ * @action: pointer of func
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
