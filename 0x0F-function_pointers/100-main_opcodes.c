c5251de7bbd349d0116d9df697b231af5828261e
dealdrain <dealdrain@yahoo.com>
2023-07-19 11:43:00 -0700

    Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,43 +1,40 @@
-#include <stdio.h>
-#include <udis86.h>
-#include <stdlib.h>
-
+#include<stdio.h>
+#include<stdlib.h>
 
 /**
-  * main - main
-  * @argc: arg count
-  * @argv: str lgt
-  *
-  * Return: nothin.
-  */
+ * main- Entry point
+ *@argc: arg count
+ *@argv: str length
+* Desc: .........
+*
+* Return: 0 always
+*/
 
 int main(int argc, char *argv[])
 {
-	ud_t ud_obj;
-	int t = 0;
-	int x = 0;
+	int a;
+	int y;
 
-	if (argc == 2)
+
+	if (argc != 2)
 	{
-		t = atoi(argv[1]);
-
-		if (t < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-
-		ud_unit(&ud_obj);
-		ud_set_input_buffer(&ud_obj, argv[1], t);
-		ud_set_mode(&ud_obj, 64);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-		while (ud_disassemble(&ud_obj))
-		{
-			printf("\t%s\n", ud_insn_hex(&ud_obj));
-		}
+		printf("Error\n");
+		return (1);
+	}
+	y = atoi(argv[1]);
+	if (y < 0)
+	{
+		printf("Error\n");
+		exit(2);
 	}
 
+	for (a = 0; a < y; a++)
+	{
+		printf("%02hhx", *((char *)main + a));
+		if (a < y - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
 	return (0);
 }
-
