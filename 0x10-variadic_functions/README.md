f480b67bbb30b0b936ebb16f39aa25da4a5403d3
dealdrain <dealdrain@yahoo.com>
2023-07-20 03:26:27 -0700

    mains.c

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,3 @@
+a new read me for the variadic function project
+
+0x10. C - Variadic functions
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef VAR_FUNC
+#define VAR_FUNC
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _putchar(char c)
+
+ENDIF
