16ca7e466f149fd92b5aea00607084cc11e93d38
dealdrain <dealdrain@yahoo.com>
2023-07-20 04:48:45 -0700

    mains edit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,5 +7,17 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
+void prichar(va_list z);
+void priint(va_list z);
+void priflo(va_list z);
+void pristr(va_list z);
+
+typedef struct checker
+{
+
+char *type;
+void (*f)();
+} checker;
+
 
 #endif
