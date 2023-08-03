8b5462b12efa8e4481b4415dab9b86c7a89f57ff
dealdrain <dealdrain@yahoo.com>
2023-08-03 04:23:19 -0700

    mains

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,7 @@
 #ifndef BIT_MANIPULATION
 #define BIT_MANIPULATION
-
+#include <studio.h>
+#include <stdlib.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
