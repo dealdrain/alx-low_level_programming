0376ee071e0c87f85165f328dc32b7e39f0d547c
dealdrain <dealdrain@yahoo.com>
2023-08-07 07:56:47 -0700

    mains

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ ** read_textfile - reads a text file and prints it to the POSIX
+ ** @filename: source file
+ ** @letters: Letters to reads and prints
+ **
+ ** Return: int
+ **/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, readed;
+	char *buff = malloc(sizeof(char *) * letters);
+	
+	if (!buff)
+		return (0);
+	if (!filename)
+		return (0);
+	
+	fd = open(filename, O_RDONLY, 0600);
+	if (fd == -1)
+		return (0);
+	
+	readed = read(fd, buff, letters);
+	write(STDOUT_FILENO, buff, readed);
+	free(buff);
+	close(fd);
+	
+	return (readed);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,12 @@
-#ifndef FILE_I/O
-#define FILE_I/O
+#ifndef FILE_I/O_H
+#define FILE_I/O_H
 
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
