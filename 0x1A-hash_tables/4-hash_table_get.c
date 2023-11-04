924c224eeaf2021bdf17190500098a4ef50d2098
dealdrain <dealdrain@yahoo.com>
2023-11-03 18:30:35 -0700

    4. >>> ht[betty]

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - gets value from a hash table
+ * @ht: Hashtable to be traversed
+ * @key: Key to search
+ * Return: value, else NULL
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *curr = ht->array[index];
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	while (curr)
+	{
+		if (strcmp(curr->key, key) == 0)
+			return (curr->value);
+
+		curr = curr->next;
+	}
+	return (NULL);
+}
