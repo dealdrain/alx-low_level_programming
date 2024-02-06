b8e2ad09337d8f9453be345d8022dfd502bdba7b
dealdrain <dealdrain@yahoo.com>
2024-02-06 10:44:10 -0800

    header

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif /* SEARCH_H */
